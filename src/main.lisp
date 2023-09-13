4f68317eb8cd2ca989bda866f0737f39fad71c45
Adrian Vazquez <adrian.vazquez@jpl.nasa.gov>
2023-09-13 06:02:47 -0700

    Toki

--- src/main.lisp
+++ src/main.lisp
@@ -1,4 +1,5 @@
 (ql:quickload "bifrost-yggdrasill")
+(ql:quickload "lparallel")
 
 (declaim (optimize (speed 0) (space 0) (debug 3)))
 (defvar debug-mode t)
@@ -86,9 +87,9 @@
 		  (VERIFY
 		   (reset-state-check-counter)
 		   (when sync-result
+			 (incf verify-counter)
 			 (if (> verify-counter verify-to-lock-good-frames)
 				 (progn
-				   (incf verify-counter)
 				   (setf state-result 'LOCK))
 				 (setf state-result 'VERIFY)))
 		   (unless sync-result
@@ -103,6 +104,11 @@
 		   (unless sync-result
 			 (reset-verify-counter)
 			 (setf state-result 'SEARCH))))
+
+		;; (print state-result)
+		;; (print state-check-counter)
+		;; (print verify-counter)
+		
 		(values state-result sync-result (lambda (frame aperture)
 										   (process-fixed-frame
 											state-check-counter
@@ -112,7 +118,8 @@
 											frame
 											:aperture aperture)))))))
 
-;(process-fixed-frame 0 0 'SEARCH (make-sync-strategy (make-sync-pattern)) #x1acffc1dFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF)
+;TODO: Check counters
+(process-fixed-frame 0 0 'VERIFY (make-sync-strategy (make-sync-pattern)) #x1acffc1dFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF)
 
 (defun emit! (message)
   (format t "~A" message))
@@ -145,8 +152,8 @@
 			 (loop for aperture in (aperture-values aperture)
 				   for res = (multiple-value-list (funcall fixed-frame-processor-continuation frame aperture))
 				   when (second res)
-					 return (cons aperture res)
-				   finally (return (cons 0 res)))))
+					 return (cons aperture res) ; Exit early
+				   finally (return (cons aperture res))))) ; Giving up
 
 	(with-slots (sync-aperture-in-bits frame-length-in-bits sync-strategy) fixed-frame-stream-type
 	  (destructuring-bind (aperture state frame next-continuation) (find-marker-with-aperture sync-aperture-in-bits)
@@ -169,7 +176,7 @@
 		  
 		  (CHECK))
 		
-		(return-from process-fixed-frame-stream (values frame state next-continuation))))))
+		(return-from process-fixed-frame-stream (values frame state (lambda (frame) (process-fixed-frame-stream fixed-frame-stream-type next-continuation frame))))))))
 
 (defun get-frame-processor (stream-type)
   (typecase stream-type
@@ -182,16 +189,32 @@
 
 (setf qq #x1acffc1dFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF)
 
-(defun monad (space-system)
+(defun monad (space-system frame-queue)
   (let* ((telemetry-metadata (slot-value space-system 'telemetry-metadata))
 		 (stream-type (when telemetry-metadata (first (slot-value telemetry-metadata 'stream-set))))
-		 (frame-stream-processor-continuation (get-fixed-frame-stream-initial-state stream-type)))
+		 (frame-stream-processor-continuation (get-fixed-frame-stream-initial-state stream-type))
+		 (frame-counter 0))
 
-	(funcall frame-stream-processor-continuation qq)
-	;(describe stream-type)
-	))
+	(loop
+	  for frame = (lparallel.queue:pop-queue frame-queue)
+	  when (null frame)
+		return :exit
+	  do
+		 (print "Got Frame")
+		 (multiple-value-bind (frame state next-continuation) (funcall frame-stream-processor-continuation frame)
+		   (incf frame-counter)
+		   (setf frame-stream-processor-continuation next-continuation)
+		   (print frame-counter)
+		   (print frame)
+		   (print state))
+	)))
 
-(monad nasa-cfs::NASA-cFS)
+
+(defparameter frame-queue (lparallel.queue:make-queue))
+(defparameter test (bt:make-thread (lambda () (monad nasa-cfs::NASA-cFS frame-queue)) :name "monad"))
+
+(lparallel.queue:push-queue qq frame-queue)
+(lparallel.queue:push-queue nil frame-queue)
 
 
 ;; (print-hex (second (process-fixed-frame 0 1 'SEARCH (make-sync-strategy) (make-sync-pattern) #x1acffc1dFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF)))
@@ -200,4 +223,55 @@
 
 
 ;; (time (process-fixed-frame-stream (make-fixed-frame-stream (make-sync-strategy) 1024) qq))
+
+
+;; (ql:quickload "lparallel")
+;; (defpackage :queue-example (:use :cl ))
+;; (in-package :queue-example)
+
+;; (setf lparallel:*kernel* (lparallel:make-kernel 10))
+
+;; (let ((queue (make-queue))
+;;       (channel (make-channel)))
+;;   (submit-task channel (lambda () (list (pop-queue queue)
+;;                                    (pop-queue queue))))
+;;   (push-queue "hello" queue)
+;;   (push-queue "world" queue)
+;;   (receive-result channel))
+
+;; (defun forever (queue channel)
+;;   (loop
+;; 	(let ((x (pop-queue queue)))
+;; 	  (print x))))
+
+;; (defparameter q (make-queue))
+;; (defparameter c (make-channel))
+
+;; (defparameter test (bt:make-thread (lambda () (forever q c))))
+
+
+;; (push-queue "hello" q)
+
+
+;; (defun print-message-top-level-fixed ()
+;;   (let ((top-level *standard-output*))
+;;     (bt:make-thread
+;;      (lambda ()
+;; 	   (loop
+;; 		 (format top-level "Hello from thread!")
+;; 		 (sleep 1)
+;; 		 ))
+;;      :name "hello"))
+;;   nil)
+
+;; (print-message-top-level-fixed)
+
+;; (defparameter *counter* 0)
+
+;; (defun test-update-global-variable ()
+;;   (bt:make-thread
+;;    (lambda ()
+;;      (sleep 1)
+;;      (incf *counter*)))
+;;   *counter*)
 
